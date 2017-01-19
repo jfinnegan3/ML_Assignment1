3323ea54d86630ac7225bd0ceb09d9b51b4a6231
Jake Finn <jfinnegan3@gatech.edu>
2017-01-18 21:04:35 -0500

    First trivial test

--- src/test/java/DecisionTreeTest.java
+++ src/test/java/DecisionTreeTest.java
@@ -1,5 +1,17 @@
 package test.java;
 
-public class DecisionTreeTest {
+import junit.framework.*;
+import main.java.DecisionTree;
 
+public class DecisionTreeTest extends TestCase {
+
+	protected void setUp() {
+		
+	}
+	
+	public void testTheTruth() {
+		assertTrue(true);
+	}
+	
+	
 }
